6a50881941654d642007bbaadef41cbd5e8bdd1c
Maria Losensky <miechen1998@web.de>
2023-12-08 14:09:46 +0100

    first commit

---
+++ meinSkript.js
@@ -0,0 +1,9 @@
+// JavaScript-Code, der durch die Schaltfläche aktiviert wird
+document.addEventListener('DOMContentLoaded', function() {
+    const meineSchaltflaeche = document.getElementById('meineSchaltflaeche');
+    
+    meineSchaltflaeche.addEventListener('click', function() {
+        // Hier können Sie den JavaScript-Code einfügen, der durch Klicken auf die Schaltfläche ausgeführt werden soll
+        alert('Die Schaltfläche wurde geklickt!');
+    });
+});
