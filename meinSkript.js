02c9be2928cf525b1a60e8cf979fc74b10c33b92
Maria Losensky <miechen1998@web.de>
2024-01-06 21:55:09 +0100

    deleted:    Fallstudie_IPWA01-01 	deleted:    Fallstudie_IPWA01-01.html 	new file:   Rest/001-2023-1113_IPWA01-01_Course_Book.pdf 	new file:   Rest/Dokument.docx 	new file:   Rest/LOGO.jpg 	new file:   Rest/Logo Idee.PNG 	new file:   Rest/Responsive Web Design Essentails.pdf 	new file:   Rest/s11616-021-00682-z.pdf 	modified:   beispiel.html 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.rtl.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.rtl.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.rtl.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-grid.rtl.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.rtl.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.rtl.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.rtl.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-reboot.rtl.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.rtl.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.rtl.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.rtl.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap-utilities.rtl.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap.min.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap.rtl.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap.rtl.css.map 	new file:   bootstrap-5.3.2-dist/css/bootstrap.rtl.min.css 	new file:   bootstrap-5.3.2-dist/css/bootstrap.rtl.min.css.map 	new file:   bootstrap-5.3.2-dist/js/bootstrap.bundle.js 	new file:   bootstrap-5.3.2-dist/js/bootstrap.bundle.js.map 	new file:   bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js 	new file:   bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js.map 	new file:   bootstrap-5.3.2-dist/js/bootstrap.esm.js 	new file:   bootstrap-5.3.2-dist/js/bootstrap.esm.js.map 	new file:   bootstrap-5.3.2-dist/js/bootstrap.esm.min.js 	new file:   bootstrap-5.3.2-dist/js/bootstrap.esm.min.js.map 	new file:   bootstrap-5.3.2-dist/js/bootstrap.js 	new file:   bootstrap-5.3.2-dist/js/bootstrap.js.map 	new file:   bootstrap-5.3.2-dist/js/bootstrap.min.js 	new file:   bootstrap-5.3.2-dist/js/bootstrap.min.js.map 	modified:   index.html 	new file:   logo_fertig.jpg 	modified:   meinSkript.js 	modified:   styles.css

--- meinSkript.js
+++ meinSkript.js
@@ -1,22 +1,34 @@
-// Ein Beispieltext mit geschlechtsspezifischer Sprache
-const inputText = "Der Lehrer sagte dem Schüler, dass er fleißig sein soll.";
-
 // Eine Funktion zur Umwandlung von geschlechtsspezifischer Sprache in genderneutrale Sprache
 function genderText(text) {
   // Ersetze "Der" durch "Die"
   text = text.replace(/\bDer\b/g, "Die");
 
-  // Ersetze "Der" durch "Die"
-  text = text.replace(/\bDer\b/g, "Die");
-
   // Ersetze "Lehrer" durch "Lehrperson"
-  text = text.replace(/\bLehrer\b/g, "Lehrperson");
+  text = text.replace(/\bLehrer\b/gi, "Lehrperson");
 
   // Ersetze "Schüler" durch "Schüler*in"
-  text = text.replace(/\bSchüler\b/g, "Schüler*in");
+  text = text.replace(/\bSchüler\b/gi, "Schüler*in");
 
   // Ersetze "er" durch "sie/er"
   text = text.replace(/\ber\b/g, "sie/er");
+
+  text = text.replace(/\bFachmann\b/gi, "Fachpersonal");
+
+  text = text.replace(/\bKäufer\b/gi, "Käufer*In");
+
+  text = text.replace(/\bMonteur\b/gi, "Montagefachkraft");
+
+  text = text.replace(/\bVerkäufer\b/gi, "Verkäufer*In");
+
+  text = text.replace(/\bMitarbeiter\b/gi, "Mitarbeiter*In");
+
+  text = text.replace(/\bExperte\b/gi, "Person mit Expertise");
+
+  text = text.replace(/\bAdministrator\b/gi, "Administrator*In");
+
+  text = text.replace(/\bAnwender\b/gi, "Anwender*In");
+
+  text = text.replace(/\bAbsender\b/gi, "Absendende Person");
 
   return text;
 }
@@ -30,98 +42,32 @@ function genderAndDisplay(elementId, displayTextId, isTextarea = false) {
   if (isTextarea) {
     originalText = element.value; // Verwende .value für Textareas
   } else {
-    //originalText = element.querySelector('p').innerText; // Text aus dem <p>-Element extrahieren
-    originalText = element.innerHTML;
+    originalText = element.querySelector('p').innerText;
   }
 
-  // const originalText = element.querySelector('p').innerText;
   const newText = genderText(originalText);
-  //element.querySelector('p').innerText = newText;
-
-  // Überprüfen, ob es sich um eine Textarea handelt
-  //  if (isTextarea) {
-    //element.value = newText; // Verwende .value für Textareas
-  //} else {
-     // element.querySelector('p').innerText = newText; // Text ins <p>-Element einfügen
- // }
-
-  // Hier wird das gemeinsame Ausgabefeld aktualisiert
-  //outputText.innerText = newText;
-  //element.querySelector('.right-section').innerText = newText;
-  //document.getElementById('displayText').innerText = newText;
-  //element.querySelector('#displayText1').innerText = newText;
 
   const displayTextElement = document.getElementById(displayTextId);
+  //console.log(newText);
   displayTextElement.innerText = newText;
 }
 
-// Funktion zur Überprüfung und Anzeige von gendergerechtem Text
-//function genderAndDisplay(elementId, isTextarea = false) {
-  // Element anhand der ID abrufen
-  //const element = document.getElementById(elementId);
-
-  // Textvariable deklarieren
-  //let text;
-
-  
-
-  // Geschlechtergerechten Text generieren
-  //const newText = genderText(text);
-
-  
-//}
-
-// Hier könnte weiterer JavaScript-Code folgen...
-
-
-/*  Eigentlich unnötig?
-Funktion zum Anwenden des Genderns auf alle Texte
-function applyGendering() {
-  const textElements = document.querySelectorAll('.text');
-  
-  textElements.forEach((element) => {
-      const originalText = element.querySelector('p').innerText;
-      const newText = genderText(originalText);
-      element.querySelector('p').innerText = newText;
-  });
-}
-*/
-
-// Event Listener für die Schaltfläche zum Ausführen des Gendering
-document.addEventListener('DOMContentLoaded', () => {
-  const genderButton = document.createElement('button');
-  genderButton.innerText = 'Gendern';
-  genderButton.addEventListener('click', applyGendering);
-
-  const footer = document.querySelector('footer');
-  footer.appendChild(genderButton);
-});
 
 // Script für die Sicherheit: Überprüfung auf injizierten Code
 function checkForInjectedCode() {
+  // Finde alle Elemente mit der Klasse 'text'
   const textElements = document.querySelectorAll('.text');
 
   textElements.forEach((element) => {
+      // Extrahiere den ursprünglichen Text aus dem ersten <p>-Element im aktuellen Element
       const originalText = element.querySelector('p').innerText;
 
       // Hier könnten weitere Überprüfungen auf injizierten Code erfolgen
 
+      // Überprüfe, ob der ursprüngliche Text das Muster '<script>' enthält
       if (originalText.includes('<script>')) {
+        // Wenn ja, zeige eine Fehlermeldung in der Konsole an
           console.error('Injizierter Code gefunden!');
       }
   });
 }
-
-// Event Listener für die Schaltfläche zur Code-Überprüfung
-document.addEventListener('DOMContentLoaded', () => {
-  const codeCheckButton = document.createElement('button');
-  codeCheckButton.innerText = 'Code überprüfen';
-  codeCheckButton.addEventListener('click', checkForInjectedCode);
-
-  const footer = document.querySelector('footer');
-  footer.appendChild(codeCheckButton);
-});
-
-// Den Text verarbeiten und ausgeben
-const resultText = genderNeutralizeText(inputText);
-console.log(resultText);
