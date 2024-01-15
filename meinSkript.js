ecb2ba46bb27583a066c091982a5f6b434adb6e0
Maria Losensky <miechen1998@web.de>
2024-01-15 16:43:16 +0100

    modified:   beispiel.html 	modified:   index.html 	modified:   kontakt.html 	modified:   meinSkript.js 	modified:   styles.css 	modified:   wandler.html

--- meinSkript.js
+++ meinSkript.js
@@ -1,34 +1,68 @@
-// Eine Funktion zur Umwandlung von geschlechtsspezifischer Sprache in genderneutrale Sprache
+// Funktion zur Umwandlung von geschlechtsspezifischer Sprache in genderneutrale Sprache
+// Hier wurden einige geschlechterspezifischen Bezeichnungen zufällig ausgewählt
+// Alle folgenden Bezeichnungen sowie Artikel sind beispielhaft gewählt (es wurden nicht alle Möglichkeiten der Nutzung in einem Satz abgedeckt)
 function genderText(text) {
-  // Ersetze "Der" durch "Die"
-  text = text.replace(/\bDer\b/g, "Die");
+  // Erklärung an dem ersten Befehl
+  // Ersetze "Lehrer" ODER "Lehrerin" durch "Lehrperson" und gleiches Prinzip mit zwei möglichen Artikeln
+  text = text.replace(/\b(Lehrer|Lehrerin)\b/gi, "Lehrperson");
+  text = text.replace(/\b(der Lehrer|die Lehrerin)\b/gi, "die Lehrperson");
+  text = text.replace(/\b(ein Lehrer|eine Lehrerin)\b/gi, "eine Lehrperson");
 
-  // Ersetze "Lehrer" durch "Lehrperson"
-  text = text.replace(/\bLehrer\b/gi, "Lehrperson");
-
-  // Ersetze "Schüler" durch "Schüler*in"
+  // folgend gleiches Prinzip wie bei "Lehrperson"
   text = text.replace(/\bSchüler\b/gi, "Schüler*in");
+  text = text.replace(/\b(der Schüler)\b/gi, "der/die Schüler*in");
 
-  // Ersetze "er" durch "sie/er"
-  text = text.replace(/\ber\b/g, "sie/er");
+  text = text.replace(/\b(Fachmann|Fachfrau)\b/gi, "Fachpersonal");
+  text = text.replace(/\b(der Fachmann|die Fachfrau)\b/gi, "das Fachpersonal");
+  text = text.replace(/\b(ein Fachmann|eine Fachfrau)\b/gi, "Fachpersonal");
 
-  text = text.replace(/\bFachmann\b/gi, "Fachpersonal");
-
-  text = text.replace(/\bKäufer\b/gi, "Käufer*In");
+  text = text.replace(/\b(Käufer|Käuferin)\b/gi, "Käufer*in");
+  text = text.replace(/\b(der Käufer|die Käuferin)\b/gi, "der/die Käufer*in");
+  text = text.replace(/\b(ein Käufer|eine Käuferin)\b/gi, "ein/eine Käufer*in");
 
   text = text.replace(/\bMonteur\b/gi, "Montagefachkraft");
+  text = text.replace(/\b(der Monteur)\b/gi, "die Montagefachkraft");
+  text = text.replace(/\b(ein Monteur)\b/gi, "eine Montagefachkraft");
 
-  text = text.replace(/\bVerkäufer\b/gi, "Verkäufer*In");
+  text = text.replace(/\b(Verkäufer|Verkäuferin)\b/gi, "Verkaufspersonal");
+  text = text.replace(/\b(der Verkäufer|die Verkäuferin)\b/gi, "das Verkaufspersonal");
+  text = text.replace(/\b(ein Verkäufer|eine Verkäuferin)\b/gi, "Verkaufspersonal");
 
-  text = text.replace(/\bMitarbeiter\b/gi, "Mitarbeiter*In");
+  text = text.replace(/\b(Mitarbeiter|Mitarbeiterin)\b/gi, "Mitarbeiter*in");
+  text = text.replace(/\b(der Mitarbeiter|die Mitarbeiterin)\b/gi, "der/die Mitarbeiter*in");
+  text = text.replace(/\b(ein Mitarbeiter|eine Mitarbeiterin)\b/gi, "ein/eine Mitarbeiter*in");
 
   text = text.replace(/\bExperte\b/gi, "Person mit Expertise");
+  text = text.replace(/\b(der Experte)\b/gi, "die Person mit Expertise");
+  text = text.replace(/\b(ein Experte)\b/gi, "eine Person mit Expertise");
 
-  text = text.replace(/\bAdministrator\b/gi, "Administrator*In");
+  text = text.replace(/\bAdministrator\b/gi, "Administrator*in");
+  text = text.replace(/\b(der Administrator)\b/gi, "der/die Administrator*in");
+  text = text.replace(/\b(ein Administrator)\b/gi, "ein/eine Administrator*in");
 
-  text = text.replace(/\bAnwender\b/gi, "Anwender*In");
+  text = text.replace(/\b(Anwender|Anwenderin)\b/gi, "Anwender*in");
+  text = text.replace(/\b(der Anwender|die Anwenderin)\b/gi, "der/die Anwender*in");
+  text = text.replace(/\b(ein Anwender|eine Anwenderin)\b/gi, "ein/eine Anwender*in");
 
   text = text.replace(/\bAbsender\b/gi, "Absendende Person");
+  text = text.replace(/\b(der Absender)\b/gi, "die absendende Person");
+  text = text.replace(/\b(ein Absender)\b/gi, "eine absendende Person");
+
+  text = text.replace(/\b(Betroffene)\b/gi, "betroffene Person");
+  text = text.replace(/\b(der Betroffene)\b/gi, "die betroffene Person");
+  text = text.replace(/\b(ein Betroffener)\b/gi, "eine betroffene Person");
+
+  text = text.replace(/\b(Student|Studentin)\b/gi, "Studierende");
+  text = text.replace(/\b(der Student|die Studentin)\b/gi, "der/die Studierende");
+  text = text.replace(/\b(ein Student|eine Studentin)\b/gi, "ein/eine Student*in");
+
+  text = text.replace(/\bInteressent\b/gi, "Interessierte");
+  text = text.replace(/\b(der Interessent)\b/gi, "der/die Interessierte");
+  text = text.replace(/\b(ein Interessent)\b/gi, "eine interessierte Person");
+
+  text = text.replace(/\bHerausgeber\b/gi, "Herausgegeben von");
+
+  text = text.replace(/\bLektor\b/gi, "Lektoriert von");
 
   return text;
 }
@@ -62,7 +96,7 @@ function checkCodeInjection(pruefText) {
   if (scriptRegex.test(pruefText) || ausdruckRegex.test(pruefText) || tagRegex.test(pruefText) || functionRegex.test(pruefText) ){
     alert(' In ihrem Text wurde potenziell gefährlicher Code entdeckt! Bitte entfernen Sie die entsprechenden Stellen und versuchen Sie es erneut.');
     document.getElementById('inputTextFrei').value = '';
-    document.getElementById('displayTextFrei').innerText = '';
+    document.getElementById('displayTextFrei').innerText = ''; //innerText statt innerHTML auch gegen injizierten Code
     return true;
   } else return false;
 }
@@ -98,6 +132,11 @@ function loremWiederholung(){
   }
   //contentElement.appendChild(paragraph);
   contentElement.innerText=text;
+}
+
+function autoResize(element) {
+  element.style.height = "auto"; // Zurücksetzen auf automatische Höhe
+  element.style.height = (element.scrollHeight) + "px"; // Anpassen der Höhe basierend auf dem Scrollhöhenwert
 }
 
 
